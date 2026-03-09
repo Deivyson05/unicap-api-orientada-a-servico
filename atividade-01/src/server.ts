8a6a7cdf1f22f01668a9778615ae85a8b99bac88
Deivyson05 <deivysonrssantos@outlook.com>
2026-03-09 18:33:41 -0300

    bug: corrige erro de importação novamente

--- atividade-01/src/server.ts
+++ atividade-01/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
-import router from "./routes/routes.ts";
+import router from "./routes/routes.js";
 
 const app: express.Application = express();
 app.use(cors());
